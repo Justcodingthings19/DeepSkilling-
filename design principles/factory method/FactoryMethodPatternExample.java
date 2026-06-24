a9c194aa8b4fb1c5a637d3bbd807bfa2de27f9a3
Lekhya Pampana <176016269+Justcodingthings19@users.noreply.github.com>
2026-06-24 15:09:25 +0530

    Add files via upload

---
+++ design principles/factory method/FactoryMethodPatternExample.java
@@ -0,0 +1,67 @@
+// FactoryMethodPatternExample.java
+
+// Document Interface
+interface Document {
+    void open();
+}
+
+// Concrete Document Classes
+class WordDocument implements Document {
+    public void open() {
+        System.out.println("Opening Word Document");
+    }
+}
+
+class PdfDocument implements Document {
+    public void open() {
+        System.out.println("Opening PDF Document");
+    }
+}
+
+class ExcelDocument implements Document {
+    public void open() {
+        System.out.println("Opening Excel Document");
+    }
+}
+
+// Abstract Factory
+abstract class DocumentFactory {
+    public abstract Document createDocument();
+}
+
+// Concrete Factories
+class WordFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new WordDocument();
+    }
+}
+
+class PdfFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new PdfDocument();
+    }
+}
+
+class ExcelFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new ExcelDocument();
+    }
+}
+
+// Test Class
+public class FactoryMethodPatternExample {
+    public static void main(String[] args) {
+
+        DocumentFactory wordFactory = new WordFactory();
+        Document wordDoc = wordFactory.createDocument();
+        wordDoc.open();
+
+        DocumentFactory pdfFactory = new PdfFactory();
+        Document pdfDoc = pdfFactory.createDocument();
+        pdfDoc.open();
+
+        DocumentFactory excelFactory = new ExcelFactory();
+        Document excelDoc = excelFactory.createDocument();
+        excelDoc.open();
+    }
+}
